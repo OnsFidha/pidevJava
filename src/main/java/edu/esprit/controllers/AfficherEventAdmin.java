634af3f095e1308a42adff45524576d0ef91115c
SyrineZaier <Syrine.zaier@esprit.tn>
2024-04-14 16:39:35 +0100

    Admin Add

--- src/main/java/edu/esprit/controllers/AfficherEventAdmin.java
+++ src/main/java/edu/esprit/controllers/AfficherEventAdmin.java
@@ -2,14 +2,20 @@ package edu.esprit.controllers;
 
 import edu.esprit.entities.Evenement;
 import edu.esprit.service.EvenementService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -59,6 +65,33 @@ public class AfficherEventAdmin implements Initializable {
             e.printStackTrace();
             // Handle any potential errors here
         }
+
+    }
+
+    @FXML
+    void Ajouter(ActionEvent event) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Ajoutereventadmin.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception gracefully
+            // Show an error message to the user
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Page Navigation Error");
+            alert.setContentText("An error occurred while navigating to the new page. Please try again.");
+            alert.showAndWait();
+        }
+
 
     }
 }
